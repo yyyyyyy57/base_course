bc0677f737f46dc583fa137078e4fc3449eda1ad
yyyyyyy57 <63540084+yyyyyyy57@users.noreply.github.com>
2024-09-25 16:06:55 +0000

    l.k.jkl

---
+++ lab2.py
@@ -0,0 +1,8 @@
+a = 'Hello, World!'
+print(type(a))
+a = 3 + 4
+print(type(a))
+a = 3 / 4
+print(type(a))
+a = [1, 2, 5, 10, 100]
+print(type(a))
---
+++ lab6.py
@@ -0,0 +1,8 @@
+age = (input('Введите Ваш возраст: '))
+sex = (input('Введите Ваш пол: '))
+name = (input('Введите Ваше имя: '))
+city = (input('Введите Ваш город: '))
+grade = (input('Введите Ваш класс: '))
+
+a = [age, sex, name, city, grade]
+print(a)
