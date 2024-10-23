09c5e2cfc0faea3fa9a8edb8f2261d88c20eef49
yyyyyyy57 <63540084+yyyyyyy57@users.noreply.github.com>
2024-10-23 16:46:22 +0000

    her6

--- task4.py
+++ task4.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+N = 10
+M = 5
+
+trigonometry_array = np.zeros((N, M))
+
+for i in range(N):
+    for j in range(M):
+        trigonometry_array[i, j] = np.sin(i*N)
+        if trigonometry_array[i, j] < 0:
+            trigonometry_array[i, j] = 0
+
+print(trigonometry_array)
