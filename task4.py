e993811f0bf36f751bbecad59b6cd3f098dd3a8b
yyyyyyy57 <63540084+yyyyyyy57@users.noreply.github.com>
2024-11-20 14:10:35 +0000

    kjnjbh

--- task4.py
+++ task4.py
@@ -1,2 +1,7 @@
+import random
+
 flowers = ['rose', 'dandelion', 'chamomile', 'tulip']
 colors = ['red', 'yellow', 'white', 'pink', 'blue', 'purple']
+
+bouquet = dict(zip(flowers, [random.choice(colors) for i in flowers]))
+print(bouquet)
